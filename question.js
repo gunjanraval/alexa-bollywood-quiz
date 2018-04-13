cc6a00e1a02edb3f04ce1f4e21cac081b45f8201
Gunjan Rawal <gunjan.bda1524@ict.gnu.ac.in>
2018-04-13 14:20:50 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,296 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_US: [
+     {
+            'Which of these films was Ashutosh Gowariker\'s directorial debut?': [
+                'Pehla Nasha',
+                'Lagaan',
+                'Afsana Pyaar Ka',
+                'Jung',
+            ],
+},
+
+{
+            'In which year did Amitabh Bachchan debut in Hindi cinema?': [
+                '1972',
+                '1968',
+                '1969',
+                '1965',
+                '1996',
+            ],
+},
+
+{
+            'What are John Abraham and Akshay Kumar\'s professions in Garam Masala?': [
+                'Photographers',
+                'Reporters',
+                'Professors',
+                'Lawyers',
+            
+            ],
+},
+
+{
+            'From where does Veeru propose to Basanti in Sholay?': [
+                'Top of a water tank',
+                'Top of a roof',
+                'Top of a hill',
+                'Top of a ladder',
+            
+            ],
+},
+
+{
+            'Who, apart from Aamir Khan, wants to marry Preity Zinta in Dil Chahta Hai?': [
+                'Ayub Khan',
+                'Shah Rukh Khan',
+                'Saif Ali Khan',
+                'Akshaye Khanna',
+            
+            ],
+},
+
+
+{
+            'What is Shah Rukh\'s mantra to woo a girl in Kal Ho Naa Ho?': [
+                'Che din ek ladki',
+                'Ek din ek ladki',
+                'Saat din ek ladki',
+                'Do din ek ladki',
+            
+            ],
+},
+
+{
+            'Who plays the blind friend in the 1964 Dosti?': [
+                'Mohan',
+                'Dinu',
+                'Ramu',
+                'Shamu',
+            
+            ],
+},
+
+
+{
+            'The catch-line \'American dream, Indian soul\' was of the film...?': [
+                'Pardes',
+                'Aa Ab Laut Chalein',
+                'American Desi',
+                'Indian',
+            
+            ],
+},
+
+{
+            'Madhuri Dixit\'s name in N Chandra\'s \'Tezaab\' was...?': [
+                'Mohini',
+                'Madhuri',
+                'Pooja',
+                'Ritu',
+            
+            ],
+},
+
+
+{
+            'Aishwarya Rai was crowned Miss World in which year?': [
+                '1994',
+                '1993',
+                '1992',
+                '1990',
+            
+            ],
+},
+
+{
+            'Who was the male lead in the Sharmila Tagore-Shabana Azmi starrer Doosri Dulhan?': [
+                'Victor Banerjee',
+                'Dharmendra',
+                'Jeetendra',
+                'Sanjeev Kumar',
+            
+            ],
+},
+
+{
+            'Lata Mangeshkar was awarded the Padma Bhushan in which year?': [
+                '1969',
+                '1959',
+                '1979',
+                '1989',
+            
+            ],
+},
+
+{
+            ' Which of these actors has never appeared in television advertisements?': [
+                'Anil Kapoor',
+                'Govinda',
+                'Hrithik Roshan',
+                'Shah Rukh Khan',
+            
+            ],
+},
+
+{
+            '  Dilip Kumar-Meena Kumari starrer Yahudi was directed by...?': [
+                'Bimal Roy',
+                'Mehboob',
+                'Guru Dutt',
+                'Raj Kapoor',
+            
+            ],
+},
+
+{
+            'The aboriginal instrument used in the music of Dil Chahta Hai was...?': [
+                'Didgeridoo',
+                'Bagpiper',
+                'Clarinet',
+                'None of the above',
+            
+            ],
+},
+
+
+{
+            'Name the film in which Shahrukh Khan played the role of a Ghost': [
+                'Paheli',
+                'Veer Zaara',
+                'Om Shanti Om',
+                'Billu',
+            
+            ],
+},
+
+{
+            'Name the film in which Shahrukh Khan played the role of a Ghost': [
+                'Paheli',
+                'Veer Zaara',
+                'Om Shanti Om',
+                'Billu',
+            
+            ],
+},
+{
+            'In which film did Kajol play the other woman in her real life hubby Ajay Devgn’s life?': [
+                'Dil Kya Kare',
+                'Gundaraj',
+                'Raju Chacha',
+                'U me aur Hum',
+            
+            ],
+},
+{
+            'Which of these films had Kajol in a double role?': [
+                'Dushman',
+                'Duplicate',
+                'Dilwale Dulhania Le Jaayenge',
+                'Diljale',
+            
+            ],
+},
+{
+            'Which is the first film produced by Shahrukh Khan?': [
+                'Phir Bhi Dil Hai Hindustani',
+                'Veer Zaara',
+                'Om Shanti Om',
+                'Billu',
+            
+            ],
+},
+{
+            'The film had Saif Ali Khan and Aditya Pancholi fighting it out to for Kajol’s love. Name the film.': [
+                'Hamesha',
+                'Baazigar',
+                'Bambai ka Babu',
+                'Yeh Dillagi',
+            
+            ],
+},
+{
+            'In the film Partner, directed by David Dhavan, which actor played the lead role opposite Katrina Kaif?': [
+                'Govinda',
+                'Salman Khan',
+                'Varun Dhawan',
+                'Ranbir Kapoor',
+            
+            ],
+},
+{
+            'Who is the lead actor opposite Katrina Kaif in the film Raajneeti, a political thriller directed and produced by Prakash Jha?': [
+                'Ranbir Kapoor',
+                'Salman Khan',
+                'Varun Dhawan',
+                'Arjun Rampal',
+            
+            ],
+},
+{
+            'Which movie star has two-joint thumbs?': [
+                'Hrithik Roshan',
+                'Salman Khan',
+                'Varun Dhawan',
+                'Ranbir Kapoor',
+            
+            ],
+},
+{
+            '\'Kabhi Khushi Kabhie Gham\' released in 2001 was a big box office hit and starred many top actors of Bollywood. Who among the following did NOT act in the movie?': [
+                'Salman Khan',
+                'Shah Rukh Khan',
+                'Hrithik Roshan',
+                'Amitabh Bachchan',
+            
+            ],
+},
+{
+            'The 1990s were dominated by the Khans ; Aamir, Salman and Shahrukh. In which of the following films did Shah Rukh Khan play the role of a villain?': [
+                'Darr',
+                'Kuch Kuch Hota hai',
+                'Dilwale Dulhania Le Jaayenge',
+                'Pardes',
+            
+            ],
+},
+
+{
+            'Who is the Director of Dabangg 2?': [
+                'Arbaz Khan',
+                'Abhinav Kashyap',
+                'Anurag Kashyap',
+                'Ram Gopal Verma',
+            
+            ],
+},
+
+
+{
+            'Who is the Music Director of Rang de Basanti?': [
+                'A.R Rehman',
+                'Shankar Ehsaan Loy',
+                'Nadeem Shravan',
+                'Jatin Lalit',
+            
+            ],
+},
+
+{
+            'What was the name of Sunil Shetty\'s Character in the movie Hera Pheri?': [
+                'Shyam',
+                'Raju',
+                'BabuRao',
+                'Khadak Singh',
+            
+            ],
+},
+
+
+    ],
+};
